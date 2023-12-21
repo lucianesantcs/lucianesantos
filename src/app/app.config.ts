b2ec8d18e66b91a7a430aca3de354c3dfd290e79
Luciane Santos <lucianesantcs@gmail.com>
2023-12-21 16:06:44 -0300

    build: adicionado e configurado biblioteca de icons lucide

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,41 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  ChevronDown,
+  ChevronUp,
+  Github,
+  Linkedin,
+  LucideAngularModule,
+  Menu,
+  Moon,
+  MoveRight,
+  Sun,
+  Dribbble,
+  Mail,
+  Languages,
+  X,
+} from 'lucide-angular';
+
+const LucideIcons = {
+  MoveRight,
+  ChevronDown,
+  ChevronUp,
+  Moon,
+  Sun,
+  Github,
+  Linkedin,
+  Dribbble,
+  Mail,
+  Languages,
+  Menu,
+  X,
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(LucideAngularModule.pick(LucideIcons)),
+  ],
 };
