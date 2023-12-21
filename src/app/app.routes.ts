60648fae006ca55def7c4b74648500c14e3bf0a1
Luciane Santos <lucianesantcs@gmail.com>
2023-12-21 14:18:39 -0300

    feat: criado estrutura e componentes iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'about',
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((it) => it.AboutComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'about',
+  },
+];
