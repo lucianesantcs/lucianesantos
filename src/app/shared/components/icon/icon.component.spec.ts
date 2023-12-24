ae40b70a654fe4d9bd9d07d998f520be8ecbc29b
Luciane Santos <lucianesantcs@gmail.com>
2023-12-24 11:30:02 -0300

    test: criado testes do componente icon

--- src/app/shared/components/icon/icon.component.spec.ts
+++ src/app/shared/components/icon/icon.component.spec.ts
@@ -1,5 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  ComponentFixtureAutoDetect,
+  TestBed,
+} from '@angular/core/testing';
 
+import { LucideAngularModule, MoveRight } from 'lucide-angular';
 import { IconComponent } from './icon.component';
 
 describe('IconComponent', () => {
@@ -8,7 +13,8 @@ describe('IconComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [IconComponent],
+      imports: [IconComponent, LucideAngularModule.pick({ MoveRight })],
+      providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(IconComponent);
@@ -18,5 +24,22 @@ describe('IconComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the default value of name to move-right if no input is provided ', () => {
+    expect(component.name).toBe('MoveRight');
+  });
+
+  it('should allow setting the name input to a non-default value', () => {
+    component.name = 'Mail';
+    expect(component.name).toBe('Mail');
+  });
+
+  // it('should only allow the name input to be Lucide Icon type', () => {
+  //   // TODO: verificar valores component.name com IconType
+  // });
+
+  it('should have strokeWidth fixed size 1.5', () => {
+    expect(component.strokeWidth).toBe(1.5);
   });
 });
