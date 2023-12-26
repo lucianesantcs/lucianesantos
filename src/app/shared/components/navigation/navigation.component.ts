9a551e0e157d2ee3124ee037aa330e6dc8fadd0f
Luciane Santos <lucianesantcs@gmail.com>
2023-12-26 16:05:34 -0300

    feat: adicionado links no componente navigation

--- src/app/shared/components/navigation/navigation.component.ts
+++ src/app/shared/components/navigation/navigation.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { LinkComponent } from '../link/link.component';
+import { ILink } from '../link/link.interface';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [LinkComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  public navLinks: ILink[] = [
+    {
+      routerLink: '/about',
+      routerLinkLabel: 'Sobre',
+    },
+    {
+      routerLink: '/experience',
+      routerLinkLabel: 'Experiência',
+    },
+    {
+      routerLink: '/projects',
+      routerLinkLabel: 'Projetos',
+      showIcon: true
+    },
+    {
+      routerLink: '/contact',
+      routerLinkLabel: 'Contato',
+    },
+  ];
+}
