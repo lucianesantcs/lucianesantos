32c32ef1a06498736cf24fc5ba84dcb1212b40bc
Luciane Santos <lucianesantcs@gmail.com>
2024-02-21 20:00:08 -0300

    feat: criada página about inicial desktop

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,57 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { IconComponent } from '../../shared/components/icon/icon.component';
+import { SubTitleComponent } from '../../shared/components/sub-title/sub-title.component';
+import { TitleComponent } from '../../shared/components/title/title.component';
+import { ISocialIcons } from './about.interface';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    IconComponent,
+    ButtonComponent,
+    TitleComponent,
+    SubTitleComponent,
+  ],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  public socialIcons: ISocialIcons[] = [
+    {
+      buttonProps: {
+        showIcon: true,
+      },
+      iconProps: {
+        iconName: 'Github',
+      },
+    },
+    {
+      buttonProps: {
+        showIcon: true,
+      },
+      iconProps: {
+        iconName: 'Linkedin',
+      },
+    },
+    {
+      buttonProps: {
+        showIcon: true,
+      },
+      iconProps: {
+        iconName: 'Dribbble',
+      },
+    },
+    {
+      buttonProps: {
+        showIcon: true,
+      },
+      iconProps: {
+        iconName: 'Mail',
+      },
+    },
+  ];
 }
---
+++ src/app/pages/about/about.interface.ts
@@ -0,0 +1,7 @@
+import { IButton } from "../../shared/components/button/button.interface";
+import { IIcon } from "../../shared/components/icon/icon.interface";
+
+export interface ISocialIcons {
+    buttonProps: IButton,
+    iconProps: IIcon,
+  }
