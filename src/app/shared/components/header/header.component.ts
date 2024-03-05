71bbe233a139ffa22698c6d08a8bb772fa0a9374
Luciane Santos <lucianesantcs@gmail.com>
2024-03-05 20:34:18 -0300

    feat: adicionado evento de toggle no component toggle icon usado no header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { IRouterLink } from '../router-link/router-link.interface';
 import { ToggleIconComponent } from '../toggle-icon/toggle-icon.component';
 import { IToggleIconProps } from '../toggle-icon/toggle-icon.interface';
 import { BreakpointsService } from '../../services/breakpoints/breakpoints.service';
 import { ILink } from '../link/link.interface';
+import { IButtonEventProps } from '../button/button.interface';
 
 @Component({
   selector: 'app-header',
@@ -17,6 +18,7 @@ export class HeaderComponent {
   @Input({ required: true }) navLinks!: IRouterLink[];
   @Input() dropdownLinks!: ILink[];
   @Input() toggleIcons!: IToggleIconProps[];
+  @Output() toggleEvent = new EventEmitter<IButtonEventProps>();
 
   private breakPoints = inject(BreakpointsService);
   protected isSMbreakPoint!: boolean;
@@ -27,5 +29,9 @@ export class HeaderComponent {
       this.isSMbreakPoint = sm;
       this.isMDbreakPoint = md;
     });
+  }
+
+  public toggle(target: IButtonEventProps) {
+    this.toggleEvent.emit(target);
   }
 }
