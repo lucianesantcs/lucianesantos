8643deae7b480ed4913bd913a45230031a31ead5
Luciane Santos <lucianesantcs@gmail.com>
2024-04-14 19:19:24 -0300

    feat: criado tablet e mobile página experiência

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { DividerComponent } from '../../shared/components/divider/divider.component';
 import { RouterLinkComponent } from '../../shared/components/router-link/router-link.component';
 import { SubTitleComponent } from '../../shared/components/sub-title/sub-title.component';
 import { TitleComponent } from '../../shared/components/title/title.component';
 import { SECTIONS } from './experience.const';
+import { BreakpointsService } from '../../shared/services/breakpoints/breakpoints.service';
 
 @Component({
   selector: 'app-experience',
@@ -20,5 +21,19 @@ import { SECTIONS } from './experience.const';
   styleUrl: './experience.component.scss',
 })
 export class ExperienceComponent {
+  private breakPoints = inject(BreakpointsService);
+  protected isSMbreakPoint!: boolean;
+  protected isMDbreakPoint!: boolean;
+  protected isXLbreakPoint!: boolean;
   protected sections = SECTIONS;
+
+  constructor() {
+    this.breakPoints
+      .getBreakpoint(['sm', 'md', 'xl'])
+      ?.subscribe(({ sm, md, xl }) => {
+        this.isSMbreakPoint = sm;
+        this.isMDbreakPoint = md;
+        this.isXLbreakPoint = xl;
+      });
+  }
 }
