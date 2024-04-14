17d6d3305d83767c406be85bd4d65129634d979e
Luciane Santos <lucianesantcs@gmail.com>
2024-04-14 18:55:16 -0300

    feat: criado página experiência

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,13 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DividerComponent } from '../../shared/components/divider/divider.component';
+import { RouterLinkComponent } from '../../shared/components/router-link/router-link.component';
+import { SubTitleComponent } from '../../shared/components/sub-title/sub-title.component';
+import { TitleComponent } from '../../shared/components/title/title.component';
+import { SECTIONS } from './experience.const';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLinkComponent,
+    TitleComponent,
+    SubTitleComponent,
+    DividerComponent,
+  ],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.scss'
+  styleUrl: './experience.component.scss',
 })
 export class ExperienceComponent {
-
+  protected sections = SECTIONS;
 }
---
+++ src/app/pages/experience/experience.const.ts
@@ -0,0 +1,71 @@
+export const SECTIONS = [
+  {
+    titleContainer: {
+      title: 'Desenvolvedora FrontEnd',
+      subtitle: 'somos ed',
+    },
+    jobInfo: {
+      location: 'Rio de Janeiro, Brasil',
+      time: 'Maio 2021 - atualmente',
+    },
+    description: `Atuo no desenvolvimento de layouts a partir do Figma em HTML, SCSS, 
+    Typescript utilizando o framework Angular 15 e as melhores práticas da web. 
+    Faço manutenção de código Back-End em NodeJs + GraphQL, e com consumo de API's Rest 
+    com backend em .NET 6.0.`,
+    skills: {
+      title: 'Skills e tecnologias',
+      skills: [
+        'DevOps',
+        'HTML5',
+        'CSS',
+        'SASS',
+        'Frontend Development',
+        'Figma',
+        'Google Analytics',
+        'Web Design Responsivo',
+        'Desenvolvimento Web',
+        'TypeScript',
+        'JavaScript',
+        'GraphQL',
+        'Angular Framework 12+',
+        'Module Federation',
+        'Microfrontend com Angular',
+        'Node.js',
+      ],
+    },
+  },
+  {
+    titleContainer: {
+      title: 'Desenvolvedora FrontEnd',
+    },
+    jobInfo: {
+      location: 'Belo Horizonte, Brasil',
+      time: '2017 ~ ',
+    },
+    description: `Durante sete+ anos tenho dedicado minha jornada profissional à programação 
+    como autodidata. Concentrei meus esforços no desenvolvimento Front-End, dominando HTML, 
+    CSS e JavaScript, além de frameworks como Angular e React. Meu interesse ao mundo da 
+    tecnologia se estende ao UI Design, utilizando o Figma para criar interfaces.`,
+    skills: {
+      title: 'Skills e tecnologias',
+      skills: [
+        'HTML5',
+        'CSS',
+        'SASS',
+        'Figma',
+        'Adobe Photoshop',
+        'Illustrator',
+        'Design de Interfaces',
+        'Web Design Responsivo',
+        'JavaScript',
+        'TypeScript',
+        'Angular Framework',
+        'Node.js',
+        'JQuery',
+        'PHP',
+        'MySQL',
+        'WordPress',
+      ],
+    },
+  },
+];
