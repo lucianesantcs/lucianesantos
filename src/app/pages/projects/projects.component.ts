071b7858ff1e1103e59a44848c1088e919b102ba
Luciane Santos <lucianesantcs@gmail.com>
2024-04-26 21:07:28 -0300

    feat: criado página de projetos

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
+import { ICardProps } from '../../shared/components/card/card.interface';
 import { TitleComponent } from '../../shared/components/title/title.component';
+import { BreakpointsService } from '../../shared/services/breakpoints/breakpoints.service';
+import { CARD_PROPS } from './projects.const';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, CardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  private breakPoints = inject(BreakpointsService);
+  protected cardProps: ICardProps[] = CARD_PROPS;
+  protected isMDbreakPoint!: boolean;
+
+  constructor() {
+    this.breakPoints.getBreakpoint(['md'])?.subscribe(({ md }) => {
+      this.isMDbreakPoint = md;
+    });
+  }
+}
---
+++ src/app/pages/projects/projects.const.ts
@@ -0,0 +1,76 @@
+import { ICardProps } from '../../shared/components/card/card.interface';
+
+export const CARD_PROPS: ICardProps[] = [
+  {
+    title: 'get coffee',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'assets/images/projects.jpg',
+    buttonProps: {
+      primary: {
+        overlineLabel: 'visit github',
+        isHref: true,
+        href: 'https://github.com/lucianesantcs',
+      },
+      secondary: {
+        overlineLabel: 'visit figma',
+        isHref: true,
+        href: 'https://github.com/lucianesantcs',
+      },
+    },
+  },
+  {
+    title: 'get coffee',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'assets/images/projects.jpg',
+    buttonProps: {
+      primary: {
+        overlineLabel: 'visit github',
+        isHref: true,
+        href: 'https://github.com/lucianesantcs',
+      },
+      secondary: {
+        overlineLabel: 'visit figma',
+        isHref: true,
+        href: 'https://github.com/lucianesantcs',
+      },
+    },
+  },
+  {
+    title: 'get coffee',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'assets/images/projects.jpg',
+    buttonProps: {
+      primary: {
+        overlineLabel: 'visit github',
+        isHref: true,
+        href: 'https://github.com/lucianesantcs',
+      },
+      secondary: {
+        overlineLabel: 'visit figma',
+        isHref: true,
+        href: 'https://github.com/lucianesantcs',
+      },
+    },
+  },
+  {
+    title: 'get coffee',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+    image: 'assets/images/projects.jpg',
+    buttonProps: {
+      primary: {
+        overlineLabel: 'visit github',
+        isHref: true,
+        href: 'https://github.com/lucianesantcs',
+      },
+      secondary: {
+        overlineLabel: 'visit figma',
+        isHref: true,
+        href: 'https://github.com/lucianesantcs',
+      },
+    },
+  },
+];
