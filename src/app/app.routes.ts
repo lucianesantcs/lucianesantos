f6ad7ec8b9f8e81c411d1b255b58c3573c5df4fd
Luciane Santos <lucianesantcs@gmail.com>
2024-04-26 22:36:13 -0300

    feat: criada página de contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { ExperienceComponent } from './pages/experience/experience.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'projects',
     component: ProjectsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
   {
     path: '',
---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject } from '@angular/core';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { IButtonEventProps } from '../../shared/components/button/button.interface';
+import { DividerComponent } from '../../shared/components/divider/divider.component';
+import { TitleComponent } from '../../shared/components/title/title.component';
+import { BreakpointsService } from '../../shared/services/breakpoints/breakpoints.service';
+import { ISocialIcons, ISocialLinksMapper } from '../about/about.interface';
+import { SOCIAL_ICONS } from './contact.const';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [TitleComponent, ButtonComponent, DividerComponent],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss',
+})
+export class ContactComponent {
+  private breakPoints = inject(BreakpointsService);
+  protected socialIcons: ISocialIcons[] = SOCIAL_ICONS;
+  protected isMDbreakPoint!: boolean;
+  protected isXLbreakPoint!: boolean;
+
+  constructor() {
+    this.breakPoints.getBreakpoint(['md'])?.subscribe(({ md, xl }) => {
+      this.isMDbreakPoint = md;
+      this.isXLbreakPoint = xl;
+    });
+  }
+
+  protected socialIconRedirect(icon: IButtonEventProps): void {
+    const urls = {
+      Github: 'https://github.com/lucianesantcs',
+      Linkedin: 'https://www.linkedin.com/in/lucianesantcs/',
+      Dribbble: 'https://dribbble.com/lucianesantos',
+    };
+
+    window.open(
+      urls[icon.iconProps?.iconName as keyof ISocialLinksMapper],
+      '_blank',
+    );
+  }
+}
