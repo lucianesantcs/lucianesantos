5863c23dd7dc265d2eff70c50db2d53fa7447474
Luciane Santos <lucianesantcs@gmail.com>
2024-09-17 11:57:10 -0300

    build: configurado sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './styles/reset.css';
-import './styles/index.css';
+import './assets/styles/main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import sass from 'sass';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
     },
   },
 });
